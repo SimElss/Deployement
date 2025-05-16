eef60492cd922b88e6f57e56d91146c67437fe06
SimElss <79011788+SimElss@users.noreply.github.com>
2025-05-16 10:11:44 +0200

    Update main.py

--- project/main.py
+++ project/main.py
@@ -7,8 +7,6 @@ from app.app import handle_exit
 
 if __name__ == "__main__":
     init()
-    signal.signal(signal.SIGINT, handle_exit)
-    signal.signal(signal.SIGTERM, handle_exit)
     port = int(os.environ.get("PORT", 8000))  # 8000 est la valeur par défaut si PORT n'est pas défini
     uvicorn.run("app.app:app", host="0.0.0.0", port=port)
     
